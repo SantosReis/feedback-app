bf66fddd531b45f1cebdca772fc04b62ec38f0cb
Marcus Reis <marcus.reis@askblue.pt>
2023-08-10 22:43:46 +0100

    Fade Animation With Framer Motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import {motion, AnimatePresence} from 'framer-motion'
 import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
 
@@ -5,6 +6,28 @@ function FeedbackList({ feedback, handleDelete }) {
     if (!feedback || feedback.lenght === 0) {
         return <p>No Feedback Yet</p>
     }
+
+    return (
+        <div className="feedback-list">
+            <AnimatePresence>
+                {feedback.map((item) => (
+                    <motion.div 
+                        key={item.id}
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <FeedbackItem 
+                            key={item.id} 
+                            item={item} 
+                            handleDelete={handleDelete}
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
+        </div>
+    )
+    /*
     return (
         <div className="feedback-list">
             {feedback.map((item) => (
@@ -15,7 +38,7 @@ function FeedbackList({ feedback, handleDelete }) {
                 />
             ))}
         </div>
-    )
+    )*/
 }
 
 FeedbackList.propTypes = {
