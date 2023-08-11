14b729ac18fc6e9b627cc46094c062ec157341ed
Marcus Reis <marcus.reis@askblue.pt>
2023-08-11 22:42:08 +0100

    Create a Context & Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { v4 as uuidv4} from 'uuid'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {useState} from 'react'
+import { useState } from 'react'
 import Header from "./components/Header";
 import FeedbackList from './components/FeedbackList';
 import FeedbackStats from './components/FeedbackStats';
@@ -8,6 +8,7 @@ import FeedbackData from './data/FeedbackData';
 import FeedbackForm from './components/FeedbackForm';
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
+import { FeedbackProvider } from './context/FeedbackContext';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
@@ -25,25 +26,27 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-      <Routes>
-        <Route 
-          exact 
-          path='/' 
-          element={
-          <>
-            <FeedbackForm handleAdd={addFeedback} />
-            <FeedbackStats feedback={feedback} />
-            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-          </>
-        } />
-        <Route path='/about' element={<AboutPage />} />
-      </Routes>
-      <AboutIconLink />
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+        <Routes>
+          <Route 
+            exact 
+            path='/' 
+            element={
+            <>
+              <FeedbackForm handleAdd={addFeedback} />
+              <FeedbackStats feedback={feedback} />
+              <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+            </>
+          } />
+          <Route path='/about' element={<AboutPage />} />
+        </Routes>
+        <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
   );
 }
 
